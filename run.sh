0a8dac94830d35d2a9546c89bd68c85665e938ae
ardrahan <51294887+ardrahan@users.noreply.github.com>
2019-06-02 16:00:23 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -56,7 +56,7 @@ elif [[ $OPTION = "backup" ]]; then
 
 
   echo "Executing /usr/bin/duplicity --encrypt-key $GPG_ENCRYPT_KEY --sign-key $GPG_SIGN_KEY /data $S3PATH --archive-dir /archive --allow-source-mismatch --gpg-options '--trust-model always'"| tee -a $LOG
-  /usr/bin/duplicity --encrypt-key $GPG_ENCRYPT_KEY --sign-key $GPG_SIGN_KEY /data $S3PATH --archive-dir /archive --allow-source-mismatch --gpg-options "--trust-model always"
+  /usr/bin/duplicity --encrypt-key $GPG_ENCRYPT_KEY --sign-key $GPG_SIGN_KEY /data $S3PATH --archive-dir /archive --allow-source-mismatch --gpg-options "--trust-model always" --log-file $LOG -v9
   #/usr/local/bin/s3cmd sync $S3CMDPARAMS /data/ $S3PATH 2>&1 | tee -a $LOG
   rm -f $LOCKFILE
   echo "Finished: $(date)" | tee -a $LOG
