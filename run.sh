29188fec355616db86f2eee420c33990d333687b
ardrahan <51294887+ardrahan@users.noreply.github.com>
2019-06-02 14:49:25 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -54,7 +54,7 @@ elif [[ $OPTION = "backup" ]]; then
 
 
   echo "Executing /usr/bin/duplicity --encrypt-key $ENCRYPT_KEY --sign-key $ENCRYPT_KEY --volsize 100 /data $S3PATH --archive-dir /archive" | tee -a $LOG
-  /usr/bin/duplicity --encrypt-key $ENCRYPT_KEY --sign-key $ENCRYPT_KEY /data $S3PATH --archive-dir /archive
+  /usr/bin/duplicity --encrypt-key $ENCRYPT_KEY --sign-key $ENCRYPT_KEY /data $S3PATH --archive-dir /archive --allow-source-mismatch
   #/usr/local/bin/s3cmd sync $S3CMDPARAMS /data/ $S3PATH 2>&1 | tee -a $LOG
   rm -f $LOCKFILE
   echo "Finished: $(date)" | tee -a $LOG
