b98817f6786b267af5669a9d3a8825c2748692ad
ardrahan <51294887+ardrahan@users.noreply.github.com>
2019-06-02 15:39:58 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,8 +8,9 @@ OPTION="$1"
 ACCESS_KEY=${ACCESS_KEY:?"ACCESS_KEY required"}
 SECRET_KEY=${SECRET_KEY:?"SECRET_KEY required"}
 S3PATH=${S3PATH:?"S3_PATH required"}
-ENCRYPT_KEY=${ENCRYPT_KEY:?"ENCRYPT_KEY required"}
-PASSPHRASE=${PASSPHRASE:?"PASSPHRASE required"}
+GPG_ENCRYPT_KEY=${GPG_ENCRYPT_KEY:?"GPG_ENCRYPT_KEY required"}
+GPG_SIGN_KEY=${GPG_SIGN_KEY:?"GPG_SIGN_KEY required"}
+GPG_SIGN_KEY_PASSPHRASE=${GPG_SIGN_KEY_PASSPHRASE:?"GPG_SIGN_KEY_PASSPHRASE required"}
 
 CRON_SCHEDULE=${CRON_SCHEDULE:-0 * * * *}
 
@@ -54,7 +55,7 @@ elif [[ $OPTION = "backup" ]]; then
 
 
   echo "Executing /usr/bin/duplicity --encrypt-key $ENCRYPT_KEY --sign-key $ENCRYPT_KEY --volsize 100 /data $S3PATH --archive-dir /archive" | tee -a $LOG
-  /usr/bin/duplicity --encrypt-key $ENCRYPT_KEY --sign-key $ENCRYPT_KEY /data $S3PATH --archive-dir /archive --allow-source-mismatch
+  /usr/bin/duplicity --encrypt-key $GPG_ENCRYPT_KEY --sign-key $GPG_SIGN_KEY /data $S3PATH --archive-dir /archive --allow-source-mismatch
   #/usr/local/bin/s3cmd sync $S3CMDPARAMS /data/ $S3PATH 2>&1 | tee -a $LOG
   rm -f $LOCKFILE
   echo "Finished: $(date)" | tee -a $LOG
